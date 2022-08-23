424596377cd5149e46ab83afa25475afc651da92
Bartosz Kaszowski <bartosz@kaszowski.eu>
2022-08-23 23:41:26 +0200

    Adding modal CSS and open/close modal event

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+const openEls = document.querySelectorAll("[data-open]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+    el.addEventListener("click", function () {
+        const modalId = this.dataset.open;
+        document.getElementById(modalId).classList.add(isVisible);
+    });
+}
+
+
+document.addEventListener("click", e => {
+    if (e.target == document.querySelector(".modal.is-visible")) {
+        document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
+});
+document.addEventListener("click", e => {
+    if (e.target == document.querySelector(".close-modal")) {
+        document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
+});
+
+document.addEventListener("keyup", e => {
+    if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+        document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
+});
